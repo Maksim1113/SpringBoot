5f0e952001465a0ca50c811236ada85c2a482cf4
Maksim Khaustov <haustovmaksim13@gmail.com>
2021-01-06 17:12:16 +0300

    update_4

--- src/main/java/ru/khaustov/springcrud/controllers/UserController.java
+++ src/main/java/ru/khaustov/springcrud/controllers/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import ru.khaustov.springcrud.models.UserModel;
 import ru.khaustov.springcrud.service.UserService;
 import ru.khaustov.springcrud.service.UserServiceImp;
@@ -11,14 +13,25 @@ import ru.khaustov.springcrud.service.UserServiceImp;
 import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
     @Autowired
-    UserService userService;
+    private UserService userService;
 
-    @GetMapping("/users")
+    @GetMapping()
     public String getAllUsers(Model model){
         List<UserModel> users = userService.getAllUsers();
         model.addAttribute("users", users);
         return "getUsers";
     }
+
+    @GetMapping("/{id}")
+    public String user(@PathVariable("id") long id, Model model){
+        UserModel userModel = userService.getUser(id);
+        model.addAttribute("user", userModel);
+        return "getUser";
+
+    }
+
+
 }
